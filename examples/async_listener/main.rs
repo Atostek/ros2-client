2c6be07515f0d4a659542ee4e2fd2f5009090b14
Juhana Helovuo <juhana.helovuo@atostek.com>
2025-11-05 15:09:43 +0200

    Examples async_talker and async_listener: Ensure destructors are run and other Nodes are notifed of exiting.

--- examples/async_listener/main.rs
+++ examples/async_listener/main.rs
@@ -1,4 +1,6 @@
 use futures::StreamExt;
+use smol::future::{self, FutureExt};
+use async_ctrlc::CtrlC;
 use ros2_client::{ros2::policy, *};
 use rustdds::DomainParticipantStatusEvent;
 
@@ -6,6 +8,15 @@ pub fn main() {
   // Here is a fixed path, so this example must be started from
   // RustDDS main directory
   log4rs::init_file("examples/async_listener/log4rs.yaml", Default::default()).unwrap();
+
+  // We need to set a Ctrl-C-handler to prevent immediate stopping of the process
+  // at signal. If this is omitted, then destructors cannot run and RustDDS is
+  // unable to notify its peers over the network that it is exiting.
+  //
+  // If this is not done, the program will still run, but the ROS2 Node will just
+  // vanish from the network on exit. This causes the other ROS2 nodes to think it
+  // is present for some time afterwards.
+  let ctrl_c_signal = CtrlC::new().expect("cannot create Ctrl+C handler?");
 
   let context = Context::new().unwrap();
   let unique_node_name = format!("listener_{}", std::process::id());
@@ -16,13 +27,17 @@ pub fn main() {
     )
     .unwrap();
 
-  smol::spawn(node.spinner().unwrap().spin()).detach();
+  // Use a local excutor instead of global. This allows executor and its tasks,
+  // notably the Spinner to be dropped before we drop the Context and
+  // DomainParticipant. This again allows DomainParticipant .drop() to run
+  // and RustDDS can notify its peers about shutdown.
+  let executor = smol::Executor::new();
+  executor.spawn(node.spinner().unwrap().spin()).detach();
 
   let status_event_stream = node.status_receiver().for_each(|event| async move {
     match event {
       NodeEvent::DDS(DomainParticipantStatusEvent::RemoteWriterMatched {
-        local_reader,
-        remote_writer,
+        remote_writer, ..
       }) if remote_writer.entity_id.kind().is_user_defined() => {
         println!("Matched remote writer {remote_writer:?}");
       }
@@ -32,7 +47,7 @@ pub fn main() {
       _ => {}
     }
   });
-  smol::spawn(status_event_stream).detach();
+  executor.spawn(status_event_stream).detach();
 
   let reliable_qos = ros2::QosPolicyBuilder::new()
     .history(policy::History::KeepLast { depth: 10 })
@@ -69,5 +84,5 @@ pub fn main() {
     "wow. very listening. such topics. much subscribe."
   );
 
-  smol::block_on(subscription_stream);
+  future::block_on(executor.run(ctrl_c_signal.race(subscription_stream)));
 }
--- examples/async_talker/main.rs
+++ examples/async_talker/main.rs
@@ -1,3 +1,5 @@
+use smol::future::{self, FutureExt};
+use async_ctrlc::CtrlC;
 use ros2_client::{ros2, ros2::policy, Context, MessageTypeName, Name, NodeName, NodeOptions};
 use async_io::Timer;
 
@@ -5,6 +7,8 @@ fn main() {
   // Here is a fixed path, so this example must be started from
   // RustDDS main directory
   log4rs::init_file("examples/async_talker/log4rs.yaml", Default::default()).unwrap();
+
+  let ctrl_c_signal = CtrlC::new().expect("cannot create Ctrl+C handler?");
 
   let context = Context::new().unwrap();
 
@@ -16,7 +20,14 @@ fn main() {
     )
     .unwrap();
 
-  smol::spawn(node.spinner().unwrap().spin()).detach();
+  // Do not use the global executor, but create a local one insted.
+  // This drops the executor, and therefore spawned tasks, including spinner,
+  // before the end of the program. This gives a chance to run .drop() in
+  // `context` and therefore DomainParticipant inside it. This allows
+  // RustDDS to send RTPS messages indicating that it is exiting.
+  let executor = smol::Executor::new();
+  executor.spawn(node.spinner().unwrap().spin()).detach();
+  //smol::spawn(node.spinner().unwrap().spin()).detach();
 
   let reliable_qos = ros2::QosPolicyBuilder::new()
     .history(policy::History::KeepLast { depth: 10 })
@@ -42,7 +53,7 @@ fn main() {
   let filler = "All work and no play makes ROS a dull boy.";
   //" All play and no work makes RTPS a mere toy. ";
 
-  smol::block_on(async {
+  future::block_on(executor.run(ctrl_c_signal.race(async {
     loop {
       count += 1;
       let message = format!("count={count} {filler}");
@@ -50,5 +61,5 @@ fn main() {
       let _ = chatter_publisher.async_publish(message).await;
       Timer::after(std::time::Duration::from_secs(2)).await;
     }
-  });
+  })));
 }
